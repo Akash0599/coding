f906ec2cc1ba537656268dbcdb3e55a6caf5f45e
akash <dakash0599@gmail.com>
2021-01-21 13:07:49 +0530

    initial commit

---
+++ practice.py
@@ -0,0 +1,17 @@
+#swapping
+x = 5
+y = 10
+
+temp = x
+x = y
+y = temp
+
+print('The value of x after swapping: {}'.format(x))
+print('The value of y after swapping: {}'.format(y))
+#calender
+import calendar
+
+yy = int(input("Enter year: "))
+mm = int(input("Enter month: "))
+
+print(calendar.month(yy, mm))
